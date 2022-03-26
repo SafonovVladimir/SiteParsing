20727cd83fa7e6892fbaa6e240c94722d0a0c2e7
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-26 22:32:56 +0200

    SiteParsing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import parser_class
 
 parser = parser_class.Parser(
-    'https://www.f1news.ru/', 'C:\\Users\\restg\\PycharmProjects\\SiteParsing'
+    'https://www.f1news.ru/', 'f1news'
 )
 
 parser.run()
